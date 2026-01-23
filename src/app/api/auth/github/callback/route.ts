9f9f579cfaac2541989ce8d56d213dc13ec5d821
lywoo00 <lywoo00@naver.com>
2026-01-23 16:26:23 +0900

    feat: github 로그인, httponlt 쿠키 저장 로직 구현(#42)

--- src/app/api/auth/github/callback/route.ts
+++ src/app/api/auth/github/callback/route.ts
@@ -1,7 +1,40 @@
-// src/app/api/auth/github/callback/route.ts
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
-  // 여기에 GitHub 콜백 로직을 구현할 예정
-  return NextResponse.json({ message: 'GitHub Callback' });
+export async function GET(req: NextRequest) {
+  const code = req.nextUrl.searchParams.get('code');
+
+  if (!code) {
+    return NextResponse.json({ messege: '코드가 없습니다.' }, { status: 400 });
+  }
+
+  // 받은 code를 access token으로 교환
+  const tokenRes = await fetch('https://github.com/login/oauth/access_token', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    }),
+  });
+  const tokenData = await tokenRes.json();
+  const accessToken = tokenData.access_token;
+
+  if (!accessToken) {
+    return NextResponse.json({ messege: '토큰 에러' }, { status: 400 });
+  }
+  const response = NextResponse.redirect(new URL('/', req.url));
+
+  response.cookies.set({
+    name: 'github_access_token',
+    value: accessToken,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+    path: '/',
+  });
+  return response;
 }
