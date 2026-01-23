b220863051819db9840a469dabd37ab1c41f4616
lywoo00 <lywoo00@naver.com>
2026-01-23 15:12:09 +0900

    feat: 깃허브 콜백 라우트 임시 코드 추가(#39)

--- src/app/api/auth/github/callback/route.ts
+++ src/app/api/auth/github/callback/route.ts
@@ -1,5 +1,7 @@
-// import { cookies } from 'next/headers';
+// src/app/api/auth/github/callback/route.ts
+import { NextResponse } from 'next/server';
 
-// export async function GET(req: Request) {
-//     cookies().set('accessToken', access)
-// }
+export async function GET() {
+  // 여기에 GitHub 콜백 로직을 구현할 예정
+  return NextResponse.json({ message: 'GitHub Callback' });
+}
