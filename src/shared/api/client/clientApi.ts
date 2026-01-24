284f067bf87b16ee51502e4c8f03eda7fbc56a67
yujinimda <s0912135@gmail.com>
2026-01-24 23:24:34 +0900

    chore: clientApi에 credentials 옵션 명확화 (#45)

--- src/shared/api/client/clientApi.ts
+++ src/shared/api/client/clientApi.ts
@@ -1,11 +1,22 @@
-export async function clientApi(path: string) {
+import { ApiError } from '@/shared/api/error/ApiError';
+
+export async function clientApi<T>(path: string): Promise<T> {
   const res = await fetch(`/api/${path}`, {
+    // 로그인 쿠키를 API 요청에 같이 실어 보내는 옵션
     credentials: 'include',
   });
 
   if (!res.ok) {
-    const text = await res.text(); // JSON 가정 ❌
-    throw new Error(text);
+    let message = 'Request failed';
+
+    try {
+      const data = await res.json();
+      message = data.message ?? message;
+    } catch {
+      message = await res.text();
+    }
+
+    throw new ApiError(res.status, message);
   }
 
   return res.json();
