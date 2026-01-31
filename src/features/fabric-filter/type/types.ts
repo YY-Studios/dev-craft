3a647d51189ca4c2b4b32db9db4655310504f2b3
yujinimda <s0912135@gmail.com>
2026-01-31 14:02:06 +0900

    fix: test (#32)

--- src/features/fabric-filter/type/types.ts
+++ src/features/fabric-filter/type/types.ts
@@ -1,5 +1,4 @@
-import { TEST } from '../contants/test';
+import { TEST } from '../constants/test';
 
 export type FabricFilterKey = (typeof TEST)[number]['key'];
-
 export type FabricFilterState = Record<FabricFilterKey, boolean>;
