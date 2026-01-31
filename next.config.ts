c7c5689f833175586c543436404406dd8b535be5
지니 <117137463+yujinimda@users.noreply.github.com>
2026-01-31 14:16:39 +0900

    fix: svgr-webpack

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  turbopack: {}, // 👈 핵심: “turbopack을 의도적으로 사용 중” 표시
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
