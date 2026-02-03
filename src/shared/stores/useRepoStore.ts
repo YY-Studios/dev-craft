0d6f20e3096817a6d09abfc94eb9b3327fb73c91
lywoo00 <57310334+lywoo00@users.noreply.github.com>
2026-02-03 23:36:29 +0900

    feat: 레포지토리 선택 상태 유지 구현(#87)

--- src/shared/stores/useRepoStore.ts
+++ src/shared/stores/useRepoStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { selectedOrgType } from '@/widgets/pull-request-step/PullRequestStepWrap';
-
+import { persist, createJSONStorage } from 'zustand/middleware';
 interface RepoStore {
   selectOrg: selectedOrgType | null;
   selectRepo: string;
@@ -8,9 +8,17 @@ interface RepoStore {
   clearRepo: () => void;
 }
 
-export const useRepoStore = create<RepoStore>((set) => ({
-  selectOrg: null,
-  selectRepo: '',
-  setSelectOrg: (selectOrg, selectRepo) => set({ selectOrg, selectRepo }),
-  clearRepo: () => set({ selectOrg: null, selectRepo: '' }),
-}));
+export const useRepoStore = create<RepoStore>()(
+  persist(
+    (set) => ({
+      selectOrg: null,
+      selectRepo: '',
+      setSelectOrg: (selectOrg, selectRepo) => set({ selectOrg, selectRepo }),
+      clearRepo: () => set({ selectOrg: null, selectRepo: '' }),
+    }),
+    {
+      name: 'repo-storage',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
