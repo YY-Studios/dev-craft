8370d2536093fa7a3b2f8aa77eb38cc9f3888ad7
lywoo00 <lywoo00@naver.com>
2026-02-20 01:03:07 +0900

    feat: pr영향도 그래프 영역 스타일 수정 및 ui 수정(#145)

--- src/shared/ui/loding/LoadingAnimation.tsx
+++ src/shared/ui/loding/LoadingAnimation.tsx
@@ -5,8 +5,10 @@ import loadingAnimation from '@/../public/animations/loading.json';
 
 export const LoadingAnimation = () => {
   return (
-    <div className="w-64 h-64">
-      <Lottie animationData={loadingAnimation} loop={true} autoplay={true} />
+    <div className="flex items-center justify-center border-1 border-gray-300 rounded-md p-4">
+      <div className="w-64 h-64">
+        <Lottie animationData={loadingAnimation} loop={true} autoplay={true} />
+      </div>
     </div>
   );
 };
