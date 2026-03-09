1acbd2364386d04d8a24eca4852514e442703288
yujinimda <s0912135@gmail.com>
2026-03-10 01:20:50 +0900

    fix: 광고 스켈레톤 추가 (#00)

--- src/features/adfit/AdFitBanner.tsx
+++ src/features/adfit/AdFitBanner.tsx
@@ -1,34 +1,23 @@
 'use client';
 
-import { useEffect } from 'react';
-
-declare global {
-  interface Window {
-    kakaoAdfit?: unknown;
-  }
-}
+import Script from 'next/script';
 
 export const AdFitBanner = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = '//t1.daumcdn.net/kas/static/ba.min.js';
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
-    <div className="flex justify-center">
-      <ins
-        className="kakao_ad_area"
-        style={{ display: 'none' }}
-        data-ad-unit="DAN-uzlRejHV4SbHD0aM"
-        data-ad-width="728"
-        data-ad-height="90"
-      />
-    </div>
+    <>
+      <Script src="//t1.daumcdn.net/kas/static/ba.min.js" strategy="afterInteractive" />
+
+      <div className="flex justify-center">
+        <div className="h-[90px] w-full max-w-[728px] rounded-md bg-zinc-100">
+          <ins
+            className="kakao_ad_area block h-full w-full"
+            style={{ display: 'block' }}
+            data-ad-unit="DAN-uzlRejHV4SbHD0aM"
+            data-ad-width="728"
+            data-ad-height="90"
+          />
+        </div>
+      </div>
+    </>
   );
 };
